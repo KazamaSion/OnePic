1f48941cd3d57d3bfcc6294ccceaf57cccd62ff9
猫耳幽香 <yoshinokotori@qq.com>
2015-07-26 00:42:43 +0800

    delete node_modules

--- node_modules/vue-validator/auth.py
+++
@@ -1,3 +0,0 @@
-from hashlib import md5
-import hmac
-print ( "?auth=" + hmac.new("vue-validator:659cff54-91d1-49bc-9106-b5e5cd6d3a4f", None, md5).hexdigest())
--- node_modules/vue-validator/components/yyx990803-vue@dev/src/util/index.js
+++
@@ -1,9 +0,0 @@
-var lang   = require('./lang')
-var extend = lang.extend
-
-extend(exports, lang)
-extend(exports, require('./env'))
-extend(exports, require('./dom'))
-extend(exports, require('./misc'))
-extend(exports, require('./debug'))
-extend(exports, require('./options'))
